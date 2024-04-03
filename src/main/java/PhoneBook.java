3e11b638e09611a9129b05b496f505de71d828cc
PavelMart <martackovp@yandex.ru>
2024-04-03 12:02:53 +0300

    add findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,10 +1,22 @@
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class PhoneBook {
-    private final Map<String, String> phoneBook = new HashMap<>();
+    private final Map<String, String> phoneBook = new TreeMap<>();
     public int add(String name, String phoneNumber) {
         phoneBook.putIfAbsent(name, phoneNumber);
         return phoneBook.size();
+    }
+
+    public List<String> findByNumber(String phoneNumber) {
+        List<String> result = phoneBook
+                .entrySet()
+                .stream()
+                .filter(entry -> phoneNumber.equals(entry.getValue()))
+                .map(Map.Entry::getKey)
+                .toList();
+
+        return result.isEmpty() ? null : result;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,12 +2,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneBookTest {
     static PhoneBook phoneBook;
     @BeforeEach
     public void initializePhoneBook() {phoneBook = new PhoneBook();}
     @Test
-    public void addNameTest() {
+    public void addTest_Simple() {
         final String name = "name";
         final String phoneNumber = "77777777777";
         final int expected = 1;
@@ -16,7 +19,7 @@ public class PhoneBookTest {
     }
 
     @Test
-    public void addDifferentNamesTest() {
+    public void addTest_DifferentNames() {
         String name1 = "name1";
         String name2 = "name2";
         final String phoneNumber = "77777777777";
@@ -27,13 +30,54 @@ public class PhoneBookTest {
     }
 
     @Test
-    public void addSameNameTest() {
+    public void addTest_SameName() {
         String name1 = "name1";
         String name2 = "name1";
-        final String phoneNumber = "77777777777";
+        final String phoneNumber = "12345";
         final int expected = 1;
         phoneBook.add(name1, phoneNumber);
         final int result = phoneBook.add(name2, phoneNumber);
         Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testFindByNumber_ExistingNumber_ReturnsCorrectName() {
+        phoneBook.add("Bob", "0987654321");
+        phoneBook.add("Alice", "1234567890");
+        phoneBook.add("Aalice", "1234567890");
+
+        List<String> expected = new ArrayList<>();
+        expected.add("Bob");
+        List<String> names = phoneBook.findByNumber("0987654321");
+
+        Assertions.assertEquals(expected, names);
+    }
+
+    @Test
+    public void testFindByNumber_NonExistingNumber_ReturnsNull() {
+        phoneBook.add("Bob", "0987654321");
+        phoneBook.add("Alice", "1234567890");
+        phoneBook.add("Aalice", "1234567890");
+
+
+        List<String> names = phoneBook.findByNumber("0000000000");
+
+        Assertions.assertNull(names);
+    }
+
+    @Test
+    public void testFindByNumber_DuplicateNumber_ReturnsAllMatchingName() {
+        phoneBook.add("John", "0987654321");
+        phoneBook.add("Bob", "0987654321");
+        phoneBook.add("Alice", "1234567890");
+        phoneBook.add("Aalice", "1234567890");
+
+        List<String> expected = new ArrayList<>();
+        expected.add("Bob");
+        expected.add("John");
+
+        List<String> names = phoneBook.findByNumber("0987654321");
+
+        Assertions.assertEquals(expected, names);
     }
 }
