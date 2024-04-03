dddc9ff5b2ae47ece1636d905a0e5a9ffc028a39
PavelMart <martackovp@yandex.ru>
2024-04-03 10:49:51 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> phoneBook = new HashMap<>();
+    public int add(String name, String phoneNumber) {
+
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    static PhoneBook phoneBook;
+    @BeforeEach
+    public void initializePhoneBook() {phoneBook = new PhoneBook();}
+    @Test
+    public void addNameTest() {
+        final String name = "name";
+        final String phoneNumber = "77777777777";
+        final int expected = 1;
+        final int result = phoneBook.add(name, phoneNumber);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void addDifferentNamesTest() {
+        String name1 = "name1";
+        String name2 = "name2";
+        final String phoneNumber = "77777777777";
+        final int expected = 2;
+        phoneBook.add(name1, phoneNumber);
+        final int result = phoneBook.add(name2, phoneNumber);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void addSameNameTest() {
+        String name1 = "name1";
+        String name2 = "name1";
+        final String phoneNumber = "77777777777";
+        final int expected = 1;
+        phoneBook.add(name1, phoneNumber);
+        final int result = phoneBook.add(name2, phoneNumber);
+        Assertions.assertEquals(expected, result);
+    }
 }
